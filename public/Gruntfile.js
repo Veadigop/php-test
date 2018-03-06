d1e6ff6483216ef32d3c38491814f72a432d86ac
Iurii <veadigop@gmail.com>
2018-03-07 00:50:32 +0200

    add grunt js

--- public/Gruntfile.js
+++ public/Gruntfile.js
@@ -1,2 +1,22 @@
+module.exports = function(grunt){
+
+grunt.initConfig({
+  sass: {
+    dist: {
+      files: [{
+        expand: true,
+        cwd: 'css',
+        src: ['*.scss'],
+        dest: 'css',
+        ext: '.css'
+      }]
+    }
+  }
+});
+
+grunt.loadNpmTasks('grunt-contrib-sass');
+grunt.registerTask('default', ['sass']);
+	
+}
 
 
